1fcf9e7c104ffd649a48002d3589013358348487
Nick Olds <nolds9@yahoo.com>
2016-05-30 21:05:18 -0400

    adds jQuery starter

--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
 Using `this`, create an event listener that will print out the text content of a button when clicked. You have been provided with some starter code. Take a look at the inline code comments in `script.js` for information on where exactly you should write your code.  
 
-Things you may need to look up...
-* `.innerText`
-* `.querySelectorAll`
+You may need to look up a few helpful methods in the [jQuery docs](http://api.jquery.com/)
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // This returns an array of all the buttons on the page. Note the use of `.querySelectorAll`.
-var buttons = document.querySelectorAll( "button" );
+var buttons = $( "button" );
 
 // Use this loop to iterate through `buttons` and create an event listener for each button.
 // With `this`, you should only need to define ONE event listener.
 // Do not modify the loop itself.
-for( var i=0; i < buttons.length; i++ ){
+
+for(var i = 0; i < buttons.length; i++) {
 
 }
