22ffcc15de323781ba9dcc559259fb1a32966691
Nick Olds <nolds9@yahoo.com>
2016-05-30 21:10:13 -0400

    adds jQuery solution

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// This returns an array of all the buttons on the page. Note the use of `.querySelectorAll`.
-var buttons = document.querySelectorAll( "button" );
+// This returns an array of all the buttons on the page.
+var buttons = $("button" );
 
 // Use this loop to iterate through `buttons` and create an event listener for each button.
 // With `this`, you should only need to define ONE event listener.
 // Do not modify the loop itself.
-for( var i=0; i < buttons.length; i++ ){
-  buttons[i].addEventListener( "click", function(){
-    console.log( this.innerText );
+for(var i = 0; i < buttons.length; i++ ){
+  buttons.eq(i).on( "click", function() {
+    console.log( $(this).html() );
   });
 }
